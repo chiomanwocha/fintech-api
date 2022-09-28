6309fd76e69337e30f25bc3ef2cb84fa063fdc73
nwochachioma <nwochachioma1998@gmail.com>
2022-09-28 15:17:28 +0100

    C, U, D functionality created

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express  = require('express');
-const finTech = require('./fintech')
+let finTech = require('./fintech')
+const bodyParser = require('body-parser')
+
 const app = express();
  
 require('dotenv').config();
 const port = process.env.PORT;
 
-app.get('/fintech', (req, res) => {
+//Read (GET)
+app.get('/', (req, res) => {
     // finTech.forEach(companies =>{
     //     res.write(companies.name + '\n')
     // })
@@ -13,6 +16,46 @@ app.get('/fintech', (req, res) => {
     res.send(finTech)
 })
 
+//Find (GET)
+app.get('/:id', (req, res) =>{
+    const id = req.params.id
+    const foundFintech = finTech.find((fintechDetails) => fintechDetails.id == id )
+    res.send(foundFintech)
+})
+
+//Create (POST)
+app.use(bodyParser.json())
+app.post('/', (req, res) => {
+    const fintechDetails = req.body
+    finTech.push(fintechDetails)
+    res.send(`The fintech company with the name ${fintechDetails.name} has been added to the list !`)
+})
+
+//Update (PATCH) 
+app.patch('/:id', (req, res) =>{
+    const finTechID = req.params.id
+    const { id, name, location } = req.body
+    const updateDetails = finTech.find((finTech) => finTech.id == finTechID)
+    
+    if(id){
+        updateDetails.id = id
+    }
+    if(name){
+        updateDetails.name = name
+    }
+    if(location){
+        updateDetails.location = location
+    }
+
+    res.send(`The detail(s) of the fintech with id ${finTechID} has been updated !`)
+})
+
+//Delete (DELETE)
+app.delete('/:id', (req, res) =>{
+    const id = req.params.id
+    finTech = finTech.filter((finTech) => finTech.id != id )
+    res.send(`The fintech company with the ID ${id} has been deleted from the database`)
+})
 app.listen(port, () => {
     console.log(`Application is running on port ${port}`);
 })
